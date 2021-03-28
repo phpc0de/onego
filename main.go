72aef9742b481052d947312f19de7f3f37193632
phpc0de <46225600+phpc0de@users.noreply.github.com>
2021-03-29 01:14:08 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"onego/apis"
+)
+
+// Index returns a home page
+func Index(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"projetc_name": "test", "version": "v1", "url": "/v1"})
+}
+
+func main() {
+	// gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.GET("/", Index)
+	v1 := router.Group("v1")
+	v1.GET("/novels/:name/:source", apis.SearchNovels)
+	v1.GET("/authors/:name/:source", apis.SearchAuthors)
+	router.Run()
+}
